e9c5bf9811d91f45aabff472b38ad9d9ed41b222
Tom Castro <gkevin964@gmail.com>
2020-04-29 17:46:04 -0600

    trabajando con login, register y playlist

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,22 +1,27 @@
 import React, { useState, useEffect} from 'react'
+import {connect} from 'react-redux'
 import Search from '../Components/Search'
 import Categories from '../Components/Categories'
 import Carousel from '../Components/Carousel'
 import CarouselItem from '../Components/CarouselItem'
 import useInitialState from '../hooks/useInitialState'
 import '../assets/styles/App.scss'
-const API = 'http://localhost:3000/initalState'
 
-const Home = () =>{
-  const initialState = useInitialState(API);
-  return initialState.length === 0 ? <h1>Loading....</h1> :(
+const Home = ({myList, trends, originals}) =>{
+  return(
     <>
       <Search/>
 
-   {initialState.mylist.length > 0 &&
+   {myList.length > 0 &&
       <Categories title="Mi lista">
         <Carousel>
-          <CarouselItem/>
+          {myList.map(item=>
+            <CarouselItem
+              key={item.id}
+              {...item}
+              isList
+            />
+          )}
         </Carousel>
       </Categories>
    }
@@ -24,7 +29,7 @@ const Home = () =>{
     <Categories title="Originales de platzi video">
       <Carousel>
         {
-          initialState.originals.map(item =>
+          originals.map(item =>
             <CarouselItem key={item.id}{...item} />
         )}
       </Carousel>
@@ -33,7 +38,7 @@ const Home = () =>{
     <Categories title="Tendencias">
       <Carousel>
         {
-          initialState.trends.map(item =>
+          trends.map(item =>
             <CarouselItem key={item.id}{...item} />
         )}
       </Carousel>
@@ -41,4 +46,13 @@ const Home = () =>{
     </>
   )
 }
-export default Home;
+
+
+const mapStateToProps = state => {
+  return{
+    myList: state.myList,
+    trends: state.trends,
+    originals: state.originals,
+  }
+}
+export default connect(mapStateToProps, null)(Home)
--- src/routes/App.js
+++ src/routes/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter,Switch, Route} from 'react-router-dom'
 import Home from '../containers/Home'
 import Login from '../containers/Login'
 import Registro from '../containers/Registro'
+import Player from '../containers/Player'
 import NotFound from '../containers/NotFound'
 import Layout from '../Components/Layout'
 
@@ -13,6 +14,7 @@ const App = ()=>(
         <Route exact path="/" component={Home}/>
         <Route exact path="/login" component={Login}/>
         <Route exact path="/registro" component={Registro}/>
+        <Route exact path="/player/:id" component={Player}/>
         <Route component={NotFound}/>
       </Switch>
     </Layout>
